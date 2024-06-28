54fd9a3d6688eecc9ab7919682d16d3e4e699684
master of bait <78838999+MasterOfBait@users.noreply.github.com>
2024-06-27 23:07:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use("/", routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6969;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 discordBot.start();
